bc0aac614e19f36ef677c92e086271508a935693
MykeManalac <mrobert.manalac@gmail.com>
2026-02-11 15:54:10 +0800

    Initial Commit: Setup Express and TS

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send('Hellooooo');
+});
+
+app.listen(port,()=>{
+    console.log('listening to port');
+});
+
